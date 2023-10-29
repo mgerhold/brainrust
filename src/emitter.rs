97c00503c792c4f44b5af0ba9699d63c454a4100
Michael Gerhold <michael.gerhold@web.de>
2023-10-29 20:21:45 +0100

    refactoring

--- src/emitter.rs
+++ src/emitter.rs
@@ -1,123 +1,304 @@
+use inkwell::context::Context;
 use std::fmt::{Display, Formatter};
 use std::path::PathBuf;
 
-use inkwell::context::Context;
 use inkwell::module::Linkage;
-use inkwell::passes::{PassManager, PassManagerBuilder};
-use inkwell::targets::{
-    CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine,
-};
-use inkwell::types::BasicMetadataTypeEnum;
-use inkwell::{AddressSpace, OptimizationLevel};
 use thiserror::Error;
 
 use crate::command_line_arguments::{CommandLineArguments, EmitTarget};
+use crate::emitter::state::{Function, State};
 use crate::program::Program;
 
 #[derive(Error, Debug)]
 pub(crate) enum EmitError {
-    FailedToWriteToFile(PathBuf),
+    FailedToWriteToFile {
+        filename: PathBuf,
+        error_message: String,
+    },
+    ModuleVerificationFailed(String),
 }
 
 impl Display for EmitError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            EmitError::FailedToWriteToFile(filename) => {
+            EmitError::FailedToWriteToFile {
+                filename,
+                error_message,
+            } => {
                 write!(
                     f,
-                    "failed to write contents to file '{}'",
-                    filename.display()
+                    "failed to write contents to file '{}': {}",
+                    filename.display(),
+                    error_message
                 )
+            }
+            EmitError::ModuleVerificationFailed(error) => {
+                write!(f, "module verification failed: {error}")
             }
         }
     }
 }
 
+mod state {
+    use crate::emitter::EmitError;
+    use inkwell::builder::Builder;
+    use inkwell::context::Context;
+    use inkwell::module::Module;
+    use inkwell::passes::{PassManager, PassManagerBuilder};
+    use inkwell::targets::{
+        CodeModel, FileType, InitializationConfig, RelocMode, Target, TargetMachine,
+    };
+    use inkwell::types::{BasicType, IntType, PointerType, VoidType};
+    use inkwell::values::FunctionValue;
+    use inkwell::{AddressSpace, OptimizationLevel};
+    use std::path::Path;
+
+    pub(super) enum DataType {
+        Void,
+        Char,
+        Int,
+        Size,
+        Pointer,
+    }
+
+    pub(super) struct Function<'a> {
+        context: &'a Context,
+        builder: &'a Builder<'a>,
+        function_value: FunctionValue<'a>,
+    }
+
+    impl<'a> Function<'a> {
+        pub(super) fn new(
+            context: &'a Context,
+            builder: &'a Builder<'a>,
+            function_value: FunctionValue<'a>,
+        ) -> Self {
+            Self {
+                context,
+                builder,
+                function_value,
+            }
+        }
+
+        pub(super) fn append_basic_block(&self, name: &str) -> &'a Builder<'a> {
+            let block = self.context.append_basic_block(self.function_value, name);
+            self.builder.position_at_end(block);
+            self.builder
+        }
+    }
+
+    impl<'a> From<Function<'a>> for FunctionValue<'a> {
+        fn from(value: Function<'a>) -> Self {
+            value.function_value
+        }
+    }
+
+    pub(super) struct State<'a> {
+        pub(super) context: &'a Context,
+        pub(super) builder: Builder<'a>,
+        pub(super) module: Module<'a>,
+        target_machine: TargetMachine,
+        pub(super) void_type: VoidType<'a>,
+        pub(super) char_type: IntType<'a>,
+        pub(super) int_type: IntType<'a>,
+        pub(super) size_type: IntType<'a>,
+        pub(super) pointer_type: PointerType<'a>,
+    }
+
+    impl<'a> State<'a> {
+        pub(super) fn new(context: &'a Context, module_name: &str) -> Self {
+            let builder = context.create_builder();
+            let module = context.create_module(module_name);
+
+            let default_triplet = TargetMachine::get_default_triple();
+            Target::initialize_all(&InitializationConfig {
+                asm_parser: true,
+                asm_printer: true,
+                base: true,
+                disassembler: true,
+                info: true,
+                machine_code: true,
+            });
+            let target = Target::from_triple(&default_triplet).unwrap();
+            let target_machine = target
+                .create_target_machine(
+                    &default_triplet,
+                    TargetMachine::get_host_cpu_name().to_str().unwrap(),
+                    TargetMachine::get_host_cpu_features().to_str().unwrap(),
+                    OptimizationLevel::Aggressive,
+                    RelocMode::Default,
+                    CodeModel::Default,
+                )
+                .unwrap();
+
+            let void_type = context.void_type();
+            let char_type = context.i8_type();
+            let int_type = context.i32_type();
+            let size_type = context.ptr_sized_int_type(&target_machine.get_target_data(), None);
+            let pointer_type = char_type.ptr_type(AddressSpace::default());
+
+            Self {
+                context,
+                builder,
+                module,
+                target_machine,
+                void_type,
+                char_type,
+                int_type,
+                size_type,
+                pointer_type,
+            }
+        }
+
+        pub(super) fn module(&self) -> &Module<'a> {
+            &self.module
+        }
+
+        pub(super) fn verify(&self) -> anyhow::Result<(), EmitError> {
+            self.module
+                .verify()
+                .map_err(|error| EmitError::ModuleVerificationFailed(error.to_string()))
+        }
+
+        pub(super) fn optimize(&self) {
+            let pass_manager_builder = PassManagerBuilder::create();
+            pass_manager_builder.set_optimization_level(OptimizationLevel::Aggressive);
+
+            let module_pass_manager = PassManager::create(());
+            pass_manager_builder.populate_module_pass_manager(&module_pass_manager);
+
+            let optimized_module = module_pass_manager.run_on(&self.module);
+            dbg!(optimized_module);
+
+            let function_pass_manager = PassManager::create(&self.module);
+            pass_manager_builder.populate_function_pass_manager(&function_pass_manager);
+
+            // todo: optimize all functions individually
+            //
+            // let optimized_main_function = function_pass_manager.run_on(&main_function);
+            // dbg!(optimized_main_function);
+        }
+
+        pub(super) fn emit_assembly(&self, filename: &Path) -> anyhow::Result<(), EmitError> {
+            self.target_machine
+                .write_to_file(&self.module, FileType::Assembly, filename)
+                .map_err(|error| EmitError::FailedToWriteToFile {
+                    filename: filename.to_path_buf(),
+                    error_message: error.to_string(),
+                })
+        }
+
+        pub(super) fn emit_object_file(&self, filename: &Path) -> anyhow::Result<(), EmitError> {
+            self.target_machine
+                .write_to_file(&self.module, FileType::Object, filename)
+                .map_err(|error| EmitError::FailedToWriteToFile {
+                    filename: filename.to_path_buf(),
+                    error_message: error.to_string(),
+                })
+        }
+
+        pub(super) fn emit_llvm_ir(&self, filename: &Path) -> anyhow::Result<(), EmitError> {
+            self.module
+                .print_to_file(filename)
+                .map_err(|error| EmitError::FailedToWriteToFile {
+                    filename: filename.to_path_buf(),
+                    error_message: error.to_string(),
+                })
+        }
+    }
+}
+
+macro_rules! get_type {
+    ($state:expr, Void) => {
+        $state.void_type
+    };
+    ($state:expr, Char) => {
+        $state.char_type
+    };
+    ($state:expr, Int) => {
+        $state.int_type
+    };
+    ($state:expr, Size) => {
+        $state.size_type
+    };
+    ($state:expr, Pointer) => {
+        $state.pointer_type
+    };
+}
+
+macro_rules! add_function {
+        ($state:expr, $name:literal, [$(DataType::$parameter_type:ident),*$(,)?], DataType::$return_type:ident, $linkage:expr) => {{
+            let function_type = get_type!($state, $return_type).fn_type(&[$(get_type!($state, $parameter_type).into()),*], false);
+            let function_value = $state.module()
+                    .add_function($name, function_type, $linkage);
+            Function::new(
+                $state.context,
+                &$state.builder,
+                function_value
+            )
+        }};
+    }
+
 pub(crate) fn emit(program: &Program, arguments: &CommandLineArguments) -> anyhow::Result<PathBuf> {
-    let context = Context::create();
-    let builder = context.create_builder();
     let module_name = arguments
         .input_filename
         .file_prefix()
         .unwrap_or_default()
         .to_string_lossy()
         .to_ascii_lowercase();
-    let module = context.create_module(&module_name);
 
-    let pass_manager_builder = PassManagerBuilder::create();
-    pass_manager_builder.set_optimization_level(OptimizationLevel::Aggressive);
+    let context = Context::create();
+    let state = State::new(&context, &module_name);
 
-    let default_triplet = TargetMachine::get_default_triple();
-    Target::initialize_all(&InitializationConfig {
-        asm_parser: true,
-        asm_printer: true,
-        base: true,
-        disassembler: true,
-        info: true,
-        machine_code: true,
-    });
-    let target = Target::from_triple(&default_triplet).unwrap();
-    let target_machine = target
-        .create_target_machine(
-            &default_triplet,
-            TargetMachine::get_host_cpu_name().to_str().unwrap(),
-            TargetMachine::get_host_cpu_features().to_str().unwrap(),
-            OptimizationLevel::Aggressive,
-            RelocMode::Default,
-            CodeModel::Default,
-        )
-        .unwrap();
+    let putchar = add_function!(
+        &state,
+        "putchar",
+        [DataType::Int],
+        DataType::Int,
+        Some(Linkage::External)
+    );
+    let malloc = add_function!(
+        &state,
+        "malloc",
+        [DataType::Size],
+        DataType::Pointer,
+        Some(Linkage::External)
+    );
+    let free = add_function!(
+        &state,
+        "free",
+        [DataType::Pointer],
+        DataType::Void,
+        Some(Linkage::External)
+    );
+    let main_function = add_function!(&state, "main", [], DataType::Int, Some(Linkage::External));
 
-    let void_type = context.void_type();
-    let char_type = context.i8_type();
-    let int_type = context.i32_type();
-    let size_type = context.ptr_sized_int_type(&target_machine.get_target_data(), None);
-    let pointer_type = char_type.ptr_type(AddressSpace::default());
+    let builder = main_function.append_basic_block("entry");
 
-    let function_type = int_type.fn_type(&[], false);
-
-    let main_function = module.add_function("main", function_type, Some(Linkage::External));
-
-    let entry_block = context.append_basic_block(main_function, "entry");
-    builder.position_at_end(entry_block);
-
-    let answer = int_type.const_int(33, false);
-
-    let malloc_function_type = pointer_type.fn_type(&[size_type.into()], false);
-    let malloc_function =
-        module.add_function("malloc", malloc_function_type, Some(Linkage::External));
-    let free_function_type = void_type.fn_type(&[pointer_type.into()], false);
-    let free_function = module.add_function("free", free_function_type, Some(Linkage::External));
-
-    let putchar_function_type =
-        int_type.fn_type(&[BasicMetadataTypeEnum::IntType(int_type)], false);
-    let putchar_function =
-        module.add_function("putchar", putchar_function_type, Some(Linkage::External));
-
-    const SIZE: u64 = 1024;
-    let size = size_type.const_int(SIZE, false);
+    let answer = state.int_type.const_int(33, false);
+    let size = state.size_type.const_int(1024, false);
 
     let mem_ptr = builder
-        .build_direct_call(malloc_function, &[size.into()], "mem_ptr")
+        .build_direct_call(malloc.into(), &[size.into()], "mem_ptr")
         .unwrap()
         .try_as_basic_value()
         .unwrap_left()
         .into_pointer_value();
 
-    let index = size_type.const_int(5, false);
+    let index = state.size_type.const_int(5, false);
 
     let element_addr = unsafe {
         builder
-            .build_gep(int_type, mem_ptr, &[index], "element_addr")
+            .build_gep(state.int_type, mem_ptr, &[index], "element_addr")
             .unwrap()
     };
     builder.build_store(element_addr, answer).unwrap();
 
     let return_value = builder
         .build_direct_call(
-            putchar_function,
+            putchar.into(),
             &[builder
-                .build_load(int_type, element_addr, "loaded_val")
+                .build_load(state.int_type, element_addr, "loaded_val")
                 .unwrap()
                 .into()],
             "putchar_result",
@@ -128,35 +309,23 @@ pub(crate) fn emit(program: &Program, arguments: &CommandLineArguments) -> anyho
         .into_int_value();
 
     builder
-        .build_direct_call(free_function, &[mem_ptr.into()], "")
+        .build_direct_call(free.into(), &[mem_ptr.into()], "")
         .unwrap();
 
     let sum = builder.build_int_add(answer, return_value, "sum").unwrap();
     builder.build_return(Some(&sum)).unwrap();
 
-    module.verify().unwrap();
+    state.verify()?;
 
-    let module_pass_manager = PassManager::create(());
-    pass_manager_builder.populate_module_pass_manager(&module_pass_manager);
-
-    let function_pass_manager = PassManager::create(&module);
-    pass_manager_builder.populate_function_pass_manager(&function_pass_manager);
-
-    let optimized_main_function = function_pass_manager.run_on(&main_function);
-    dbg!(optimized_main_function);
-
-    let optimized_module = module_pass_manager.run_on(&module);
-    dbg!(optimized_module);
+    state.optimize();
 
     match arguments.emit_target() {
         EmitTarget::Assembly => {
-            target_machine
-                .write_to_file(&module, FileType::Assembly, &arguments.output_filename())
-                .unwrap();
+            state.emit_assembly(&arguments.output_filename())?;
             Ok(arguments.output_filename().clone())
         }
         EmitTarget::ObjectFile | EmitTarget::Executable => {
-            let object_file_filename = match arguments.only_compile_and_assemble {
+            let filename = match arguments.only_compile_and_assemble {
                 true => arguments.output_filename(),
                 false => {
                     let mut result = arguments.output_filename().clone();
@@ -164,16 +333,11 @@ pub(crate) fn emit(program: &Program, arguments: &CommandLineArguments) -> anyho
                     result
                 }
             };
-            target_machine
-                .write_to_file(&module, FileType::Object, &object_file_filename)
-                .map_err(|_| EmitError::FailedToWriteToFile(object_file_filename.clone()))?;
-            Ok(object_file_filename)
+            state.emit_object_file(&filename)?;
+            Ok(filename)
         }
         EmitTarget::LlvmIr => {
-            std::fs::write(
-                arguments.output_filename(),
-                module.print_to_string().to_str().unwrap(),
-            )?;
+            state.emit_llvm_ir(&arguments.output_filename())?;
             Ok(arguments.output_filename().clone())
         }
     }
